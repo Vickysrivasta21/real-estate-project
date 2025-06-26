d2182b0e3e6d103eafec152d5221e52f648f08ba
Avnish Srivastava <srivastavavicks@gmail.com>
2025-06-26 15:29:53 +0530

    made property card,added dummy json for property

---
+++ real-estate-listing-website/app/pages/Propertypage.js
@@ -0,0 +1,63 @@
+"use client";
+import React from 'react'
+import { useEffect,useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Propertypage = () => {
+    const [data,setdata] = useState([])
+    useEffect(() => {
+      let data = async()=>{
+        let property = await fetch("/propertydata.json")
+        let res = await property.json()
+        console.log(res)
+        setdata(prev=>[...prev,...res])
+      }
+      data()
+    }, [])
+    
+  return (
+    <div className='maincont'>
+    {data.map(ele=>{
+        return (
+        <div key={ele.id} className="container">
+            <div className="content">
+        <div className="image">
+        <Image
+         src={ele.image}
+         width={250}
+         height={250}
+         alt="Property image"
+         style={{objectFit:'cover'}}
+        />
+        </div>
+        {/* <div className="id">{ele.id}</div> */}
+        <div className="type-price">
+            <div className="type">
+                {ele.type}
+            </div>
+            <div className="price">
+                {ele.price}
+            </div>
+        </div>
+        <div className="title"><h1>{ele.title}</h1></div>
+        <div className="location">location : {ele.location}</div>
+        <div className="features">
+            <div className="size">{ele.size}</div>
+            <div className="beds">total bedrooms : {ele.bedrooms}</div>
+            <div className="bath">total bathroom : {ele.bathrooms}</div>
+            <div className="parking">{ele.parking?"available": "not available"}</div>
+        </div>
+        <div className="contact">
+            <div className="view"><Link href="/">view details</Link></div>
+            <div className="agent"><Link href="/">contact agent</Link></div>
+        </div>
+        </div>
+        </div>
+        )
+    })}
+    </div>
+  )
+}
+
+export default Propertypage
--- real-estate-listing-website/next.config.mjs
+++ real-estate-listing-website/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    domains: ['images.pexels.com'], // ✅ Pexels ka image domain allowed
+  },
+};
 
 export default nextConfig;
