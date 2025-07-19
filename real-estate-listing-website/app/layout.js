27b5b46e1b9e48323882e81c9cd57e1c90dc1582
mushkaan38 <muskankumari030805@gmail.com>
2025-07-20 00:12:44 +0530

    Added empty plot details

--- real-estate-listing-website/app/layout.js
+++ real-estate-listing-website/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar";
 import "react-toastify/dist/ReactToastify.css";
+import "leaflet/dist/leaflet.css";//added by muskan
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
